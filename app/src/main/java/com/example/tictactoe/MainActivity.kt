35eb7a86d90160db8a42234c506c5afce3a3adbb
parzuko <jivansh17@gmail.com>
2020-06-06 16:52:02 +0530

    Set On Click Listener

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickButton (view: View){
+
+        Toast.makeText(this,"It was Clicked!",Toast.LENGTH_SHORT).show()
     }
 }
