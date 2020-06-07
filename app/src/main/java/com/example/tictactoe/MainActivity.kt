5293c58e9618c4060db793c4abc35b394a097ef0
parzuko <jivansh17@gmail.com>
2020-06-07 19:50:59 +0530

    Add App Logic

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        game_info.text = ""
     }
 
     fun onClickButton (view: View){
@@ -49,8 +52,60 @@ class MainActivity : AppCompatActivity() {
             current_player = 1
         }
 
+        when(current_player){
+            1 -> game_info.text = "Current Player: X"
+            2 -> game_info.text = "Current Player: O"
+        }
+
         latest_button.isEnabled = false
 
+        checkWinner()
 
+        //Toast.makeText(this,"Active Player $current_player",Toast.LENGTH_SHORT).show()
+
+    }
+
+    var checkWinner = 0 // 0: no winner, 1:X,2:O
+
+    private fun checkWinner(){
+
+        // For Row 1
+        if(mover_1.contains(1) && mover_1.contains(2) && mover_1.contains(3)) {checkWinner = 1}
+        if(mover_2.contains(1) && mover_2.contains(2) && mover_2.contains(3)) {checkWinner = 2}
+
+        // For Row 2
+        if(mover_1.contains(4) && mover_1.contains(5) && mover_1.contains(6)) {checkWinner = 1}
+        if(mover_2.contains(4) && mover_2.contains(5) && mover_2.contains(6)) {checkWinner = 2}
+
+        // For Row 3
+        if(mover_1.contains(7) && mover_1.contains(8) && mover_1.contains(9)) {checkWinner = 1}
+        if(mover_2.contains(7) && mover_2.contains(8) && mover_2.contains(9)) {checkWinner = 2}
+
+        // Column 1
+        if(mover_1.contains(1) && mover_1.contains(4) && mover_1.contains(7)) {checkWinner = 1}
+        if(mover_2.contains(1) && mover_2.contains(4) && mover_2.contains(7)) {checkWinner = 2}
+
+        //Column 2
+        if(mover_1.contains(2) && mover_1.contains(5) && mover_1.contains(8)) {checkWinner = 1}
+        if(mover_2.contains(2) && mover_2.contains(5) && mover_2.contains(8)) {checkWinner = 2}
+
+        // Column 3
+        if(mover_1.contains(3) && mover_1.contains(6) && mover_1.contains(9)) {checkWinner = 1}
+        if(mover_2.contains(3) && mover_2.contains(6) && mover_2.contains(9)) {checkWinner = 2}
+
+        // Diagonal top left to bottom right
+        if(mover_1.contains(1) && mover_1.contains(5) && mover_1.contains(9)) {checkWinner = 1}
+        if(mover_2.contains(1) && mover_2.contains(5) && mover_2.contains(9)) {checkWinner = 2}
+
+        // Diagonal 2
+        if(mover_1.contains(3) && mover_1.contains(5) && mover_1.contains(7)) {checkWinner = 1}
+        if(mover_2.contains(3) && mover_2.contains(5) && mover_2.contains(7)) {checkWinner = 2}
+
+        if(checkWinner != 0){
+            when(checkWinner){
+                1 -> game_info.text = "Player X Won!!"
+                2 -> game_info.text = "Player O Won!!"
+            }
+        }
     }
 }
